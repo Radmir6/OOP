3730273602bd5fd088c93ca54cbefb3427fcf83d
QuadeLuna <6048060480dflbv>
2023-05-16 16:49:42 +0500

    first

---
+++ core/events/__init__.py
@@ -0,0 +1,15 @@
+#from .ActionGreeting import ActionGreeting
+from .ActionRussia import ActionRussia
+from .ActionPhoto import ActionPhoto
+from .ActionLocation import ActionLocation
+from .ActionVideo import ActionVideo
+from .ActionAnswer import ActionAnswer
+
+__all__ = [
+    'ActionGreeting',
+    'ActionRussia',
+    'ActionPhoto',
+    'ActionLocation',
+    'ActionVideo',
+    'ActionAnswer'
+]
